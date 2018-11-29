b584349245bf95535c55d88555b818b8219d1f89
Azarnait <azarnait@gmail.com>
2018-11-29 21:30:12 +0100

    add Slim

--- src/main/java/com/mcelep/HelloRestController.java
+++ src/main/java/com/mcelep/HelloRestController.java
@@ -9,7 +9,7 @@ public class HelloRestController {
 
     @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String index() {
-        return "Hello World " + readEnvironmentVariable() + " ! Sam&Tarik&Michi&Andreas";
+        return "Hello World " + readEnvironmentVariable() + " ! Sam&Tarik&Michi&Andreas&Slim";
     }
 
     private String readEnvironmentVariable(){
