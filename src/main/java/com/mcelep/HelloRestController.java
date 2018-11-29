fb569269f4f219e175ccf81c6560ff7c8df0c2bc
Azarnait <azarnait@gmail.com>
2018-11-29 14:03:50 +0100

    Copy of simple spring boot example

---
+++ src/main/java/com/mcelep/HelloRestController.java
@@ -0,0 +1,19 @@
+package com.mcelep;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public String index() {
+        return "Hello World " + readEnvironmentVariable() + " ! Sam&Tarik";
+    }
+
+    private String readEnvironmentVariable(){
+        String result = System.getenv("HELLO_WORLD_MSG");
+        return result==null?"":result;
+    }
+}
